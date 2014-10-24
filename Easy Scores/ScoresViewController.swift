83e6252d61436f4f9d844f7937916ed1f9bfb7df
Ian Fisher <loktar@gmail.com>
2014-10-23 17:26:26 -0700

    Started work on getting Core Data setup

--- Easy Scores/ScoresViewController.swift
+++ Easy Scores/ScoresViewController.swift
@@ -6,22 +6,53 @@
 //  Copyright (c) 2014 iFisher. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
-class ScoresViewController: UIViewController {
+class ScoresViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var playersTableView: UITableView!
+
+    var coreDataHelper = CoreDataHelper()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var e: NSError? = nil
+        if self.fetchedResultsController.performFetch(&e) {
+            NSLog("error fetching players: \(e)")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = self.fetchedResultsController.sections {
+            return sections[section].count
+        }
+        return 0
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let reuseId = "PlayerCell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(reuseId) as UITableViewCell?
+        if cell == nil {
+            cell = UITableViewCell()
+        }
+        
+        cell?.textLabel.text = "A cell"
+        
+        return cell!
+    }
+    
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        let frc = NSFetchedResultsController()
+        
+        let fetchRequest = NSFetchRequest()
+        if let moc = self.coreDataHelper.managedObjectContext {
+            let entity = NSEntityDescription.entityForName("Player", inManagedObjectContext: moc)
+            fetchRequest.entity = entity
+        }
+    
+        return frc
+    }()
 }
 
